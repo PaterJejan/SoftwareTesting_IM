cf0a44cdca3f9f30ea6c94cb97b02a09eba89b63
PaterJejan <iejan.massart@student.odisee.be>
2021-10-11 14:41:06 +0200

    update log en programma

--- Calculator/CalculatorProgram/Calculator.cs
+++ Calculator/CalculatorProgram/Calculator.cs
@@ -5,14 +5,67 @@ namespace CalculatorProgram
 {
     public class Calculator
     {
+        public decimal Current { get; set; }
+
         public decimal Sum(params decimal[] numbers)
         {
+            if (numbers.Length == 1)
+            {
+                Current += numbers[0];
+                return Current;
+            }
             var result = 0.0M;
             for (int i = 0; i < numbers.Length; i++)
             {
                 result += numbers[i];
             }
             return result;
+        }
+
+        public decimal Multiply(params decimal[] numbers)
+        {
+            ValidateNumbers(numbers);
+            if (numbers.Length == 1)
+            {
+                Current *= numbers[0];
+                return Current;
+            }
+
+            var result = numbers[0];
+            for (int i = 1; i <= numbers.Length - 1; i++)
+            {
+                result *= numbers[i];
+            }
+            return result;
+        }
+
+        private void ValidateNumbers(decimal[] numbers)
+        {
+            if (numbers.Any(number => number == decimal.MaxValue))
+            {
+                throw new InvalidOperationException("Not a valid number to use");
+            }
+        }
+
+        public decimal Divide(params decimal[] numbers)
+        {
+            if (numbers.Length == 1)
+            {
+                Current /= numbers[0];
+                return Current;
+            }
+
+            var result = numbers[0];
+            for (int i = 1; i <= numbers.Length - 1; i++)
+            {
+                result /= numbers[i];
+            }
+            return result;
+        }
+
+        public void Reset()
+        {
+            Current = 0;
         }
     }
 }
--- Calculator/UnitTests/UnitTest1.cs
+++ Calculator/UnitTests/UnitTest1.cs
@@ -4,6 +4,135 @@ using Xunit;
 
 namespace UnitTests
 {
+    public class MultiplyDivideTests
+    {
+        [Fact]
+        public void Test_Validations()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            Exception exception = Assert.Throws<InvalidOperationException>(() =>
+            {
+                // Act (the actual operation)
+                var result = calculator.Multiply(decimal.MaxValue, decimal.MaxValue);
+
+                // Then, Assert
+                Assert.IsType<decimal>(result);
+            });
+
+            Assert.Equal("Not a valid number to use", exception.Message);
+
+            exception = Assert.Throws<DivideByZeroException>(() =>
+            {
+                // Act (the actual operation)
+                var result = calculator.Divide(decimal.MaxValue, 0);
+
+                // Then, Assert
+                Assert.IsType<decimal>(result);
+            });
+
+            Assert.IsType<DivideByZeroException>(exception);
+        }
+
+        [Fact]
+        public void Test_DivideByZero()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            Assert.Throws<DivideByZeroException>(() =>
+            {
+                // Act (the actual operation)
+                var result = calculator.Divide(1, 0);
+
+                // Then, Assert
+                Assert.IsType<decimal>(result);
+            });
+        }
+
+        [Fact]
+        public void Test_MultiplyTwoElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Multiply(10, 5);
+
+            // Then, Assert
+            Assert.Equal(50, result);
+        }
+
+        [Fact]
+        public void Test_MultiplyManyElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Multiply(0.5M, 1, 2, 3, 4, -5.5M);
+
+            // Then, Assert
+            Assert.Equal(-66, result);
+            Assert.IsType<decimal>(result);
+        }
+
+        [Fact]
+        public void Test_DivideTwoElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Divide(10, 5);
+
+            // Then, Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Test_DivideManyElements()
+        {
+            // 1) Arrange
+            var calculator = new Calculator();
+
+            // Act (the actual operation)
+            var result = calculator.Divide(100, 2, 4);
+
+            // Then, Assert
+            Assert.Equal((decimal)12.5, result);
+            Assert.IsType<decimal>(result);
+        }
+    }
+
+    public class CalculatorMemoryTests : IDisposable
+    {
+        private Calculator Calculator { get; }
+
+        public CalculatorMemoryTests()
+        {
+            Calculator = new Calculator();
+        }
+
+        [Fact]
+        public void AddNumberTest()
+        {
+            Calculator.Reset();
+
+            // Act
+            Calculator.Sum(3);
+            Calculator.Sum(7);
+
+            Assert.Equal(10, Calculator.Current);
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+
     public class UnitTest1
     {
         [Fact]
